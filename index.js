47366fe52dd89b5e174062900fb03a347dc5986b
simplehima <ebrahimwaleedazab@gmail.com>
2026-01-05 16:40:00 +0200

    fix: remove duplicate path declaration

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const express = require('express');
 const session = require('express-session');
 const passport = require('passport');
 const DiscordStrategy = require('passport-discord').Strategy;
-const path = require('path');
+
 const fs = require('fs');
 
 // --- BOT SETUP ---
